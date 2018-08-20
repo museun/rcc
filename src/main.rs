7c152fae6e56ea931dfcb12062ded428aa74fd64
museun <museun@outlook.com>
2018-08-19 20:19:21 -0400

    add + and -

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,55 @@ fn main() {
         eprintln!("usage: rcc <code>");
         ::std::process::exit(1)
     }
-    let mut args = args.skip(1);
+    let args = args.skip(1).collect::<String>();
+    let mut input: &str = args.as_ref();
+
+    fn parse_next_number(input: &mut &str) -> usize {
+        let mut pos = 0;
+        let n = input
+            .chars()
+            .take_while(|c| c.is_digit(10))
+            .inspect(|_| pos += 1)
+            .collect::<String>()
+            .parse::<usize>()
+            .expect("a number");
+        *input = &input[pos..];
+        n
+    }
+
+    fn add(input: &mut &str) {
+        let n = parse_next_number(input);
+        println!("  add rax, {}", n);
+    }
+
+    fn sub(input: &mut &str) {
+        let n = parse_next_number(input);
+        println!("  sub rax, {}", n);
+    }
+
+    pub fn advance(input: &mut &str) { *input = &input[1..]; }
+
+    let data = parse_next_number(&mut input);
 
     println!(".intel_syntax noprefix");
-
     println!(".global main");
     println!("main:");
-    println!(
-        "\tmov rax, {}",
-        args.next()
-            .expect("a number")
-            .parse::<usize>()
-            .expect("a number")
-    );
+    println!("  mov rax, {}", data);
+
+    while !input.is_empty() {
+        if input.get(0..1) == Some("+") {
+            advance(&mut input);
+            add(&mut input);
+            continue;
+        }
+        if input.get(0..1) == Some("-") {
+            advance(&mut input);
+            sub(&mut input);
+            continue;
+        }
+
+        eprintln!("expected character: {:?}", &args[0..1]);
+    }
+
     println!("\tret");
 }
